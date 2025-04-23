fac2a71af6a3bdb2dd59ff9dd83fff5d21120da4
Shira Amar <shiraamaar@gmail.com>
2025-04-23 11:20:26 +0300

    add check for radius add constractor

--- src/geometries/RadialGeometry.java
+++ src/geometries/RadialGeometry.java
@@ -4,23 +4,25 @@ package geometries;
  * An abstract class that implements the geometry interface
  */
 public abstract class RadialGeometry extends Geometry {
-    /**
-     * Radius for a round shape
-     */
-    final protected double _radius;
-    /**
-     * Radius for a round shape
-     */
-    final protected double _radiusSquared;
+	/**
+	 * Radius for a round shape
+	 */
+	final protected double _radius;
+	/**
+	 * Radius for a round shape
+	 */
+	final protected double _radiusSquared;
 
-    /**
-     * Constructor to initialize the radius and calculate its square.
-     *
-     * @param radius the radius of the round shape
-     */
-    RadialGeometry(double radius) {
-        _radius = radius;
-        _radiusSquared = radius * radius;
-    }
+	/**
+	 * Constructor to initialize the radius and calculate its square.
+	 *
+	 * @param radius the radius of the round shape
+	 */
+	RadialGeometry(double radius) {
+		if (radius <= 0)
+			throw new IllegalArgumentException("Radius must be positive");
+		_radius = radius;
+		_radiusSquared = radius * radius;
+	}
 
 }
