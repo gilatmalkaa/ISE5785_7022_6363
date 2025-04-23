f7e94e85aa15e9d052160a71d41845ce6d173e5a
Shira Amar <shiraamaar@gmail.com>
2025-04-23 10:36:15 +0300

    > Added a getter and implemented the `getNormal` function.

--- src/geometries/Tube.java
+++ src/geometries/Tube.java
@@ -3,35 +3,41 @@ package geometries;
 import primitives.*;
 
 /**
- * A class that represents a tube.
- * It extends the RadialGeometry class and includes a ray defining the tube's axis.
+ * A class that represents a tube. It extends the RadialGeometry class and
+ * includes a ray defining the tube's axis.
  */
 public class Tube extends RadialGeometry {
 
-    /**
-     * The ray that defines the axis of the tube.
-     */
-    protected final Ray _ray;
+	/**
+	 * The ray that defines the axis of the tube.
+	 */
+	protected final Ray _ray;
 
-    /**
-     * Constructor to create a tube with a specified axis and radius.
-     *
-     * @param ray    the ray defining the axis of the tube
-     * @param radius the radius of the tube
-     */
-    public Tube(Ray ray, double radius) {
-        super(radius);  // Calling the constructor of RadialGeometry to set the radius
-        _ray = ray;
-    }
+	/**
+	 * Constructor to create a tube with a specified axis and radius.
+	 *
+	 * @param ray    the ray defining the axis of the tube
+	 * @param radius the radius of the tube
+	 */
+	public Tube(Ray ray, double radius) {
+		super(radius); // Calling the constructor of RadialGeometry to set the radius
+		_ray = ray;
+	}
 
-    /**
-     * Calculates the normal vector at a given point on the tube's surface.
-     * The normal vector is perpendicular to the surface of the tube at the specified point.
-     *
-     * @param point the point on the surface of the tube
-     * @return the normal vector at the given point
-     */
-    public Vector getNormal(Point point) {
-        return null;  // Placeholder, implement logic for calculating the normal
-    }
+	/**
+	 * Calculates the normal vector at a given point on the tube's surface. The
+	 * normal vector is perpendicular to the surface of the tube at the specified
+	 * point.
+	 *
+	 * @param point the point on the surface of the tube
+	 * @return the normal vector at the given point
+	 */
+	public Vector getNormal(Point point) {
+		Point p0 = _ray.getP0();
+		Vector dir = _ray.getDir();
+		Vector p0ToPoint = point.subtract(p0);
+		double t = dir.dotProduct(p0ToPoint);
+		Point o = p0.add(dir.scale(t));
+		return point.subtract(o).normalize();
+	}
 }
