6ca6e46511962529aba690d752f598ba2893b236
Shira Amar <shiraamaar@gmail.com>
2025-04-23 11:21:56 +0300

    nothing

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -6,11 +6,11 @@ import primitives.*;
  * An interface that characterizes a geometric shape
  */
 public abstract class Geometry {
-    /**
-     * Abstract method to get the normal vector at a given point.
-     *
-     * @param point the point to calculate the normal for
-     * @return the normal vector
-     */
-    public abstract Vector getNormal(Point point);
+	/**
+	 * Abstract method to get the normal vector at a given point.
+	 *
+	 * @param point the point to calculate the normal for
+	 * @return the normal vector
+	 */
+	public abstract Vector getNormal(Point point);
 }
